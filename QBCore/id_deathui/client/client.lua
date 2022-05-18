0e749ebcd87b33284bd6e0d6b2b5658eb562eb19
Infinity Development <96385278+infinitydevelopment14@users.noreply.github.com>
2022-05-18 20:54:02 +0200

    Update client.lua

--- QBCore/id_deathui/client/client.lua
+++ QBCore/id_deathui/client/client.lua
@@ -1,5 +1,5 @@
 local QBCore = exports['qb-core']:GetCoreObject()
-local IsDead = false
+local isDead = false
 local deadAnimDict = "dead"
 local deadAnim = "dead_a"
 
@@ -17,71 +17,41 @@ function SetDisplay(bool)
 	SetNuiFocus(bool, bool)
 end
 
-AddEventHandler('playerSpawned', function(spawn)
-    SetDisplay(false, false)
-    IsDead = false
+RegisterNetEvent('hospital:client:Revive', function()
+    if isDead then
+        SetDisplay(false, false)
+        isDead = false 
+    end
+end)
+
+RegisterNetEvent('hospital:client:SendToBed', function(id, data, isRevive)
+    print(isDead)
+    if isDead then
+        SetDisplay(false, false)
+        isDead = false 
+    end
 end)
 
 RegisterNUICallback("button", function(data)
     SendNUIMessage({action = 'hidebutton'})
 
-    -- You should place TriggerEvent here for sending message to all ambulance employees :)
+    TriggerServerEvent('hospital:server:ambulanceAlert', 'Civilian Down')
  
-    SetNuiFocus(false, false)
+    SetNuiFocus(true, true)
 end)
 
-function respawn()
-    SetDisplay(false, false)
-	SetEntityCoordsNoOffset(PlayerPedId(), GlobalState.RespawnCoords, false, false, false, true)
-    NetworkResurrectLocalPlayer(GlobalState.RespawnCoords, GlobalState.RespawnHeading, true, false)
-	SetPlayerInvincible(PlayerPedId(), false)
-	ClearPedBloodDamage(PlayerPedId())
-end
-
-function OnDeath()
-    if not isDead then
+RegisterNetEvent('id_deathui:client:onDeath')
+AddEventHandler('id_deathui:client:onDeath', function(dead)
+    if not isDead and dead then
         SetDisplay(true, true)
-        isDead = true
+        isDead = dead
     
         -- Respawn Player after timer is done
-        Citizen.Wait(GlobalState.Timer * 60 * 1000)
-        TriggerEvent("hospital:client:Revive")
-
-        TriggerServerEvent("id_deathui:server:SetDeathStatus", true)
-        local player = PlayerPedId()
-
-        while GetEntitySpeed(player) > 0.5 or IsPedRagdoll(player) do
-            Wait(10)
-        end
-
-        if isDead then
-            local pos = GetEntityCoords(player)
-            local heading = GetEntityHeading(player)
-
-            local ped = PlayerPedId()
-            if IsPedInAnyVehicle(ped) then
-                local veh = GetVehiclePedIsIn(ped)
-                local vehseats = GetVehicleModelNumberOfSeats(GetHashKey(GetEntityModel(veh)))
-                for i = -1, vehseats do
-                    local occupant = GetPedInVehicleSeat(veh, i)
-                    if occupant == ped then
-                        NetworkResurrectLocalPlayer(pos.x, pos.y, pos.z + 0.5, heading, true, false)
-                        SetPedIntoVehicle(ped, veh, i)
-                    end
-                end
-            else
-                NetworkResurrectLocalPlayer(pos.x, pos.y, pos.z + 0.5, heading, true, false)
+        Citizen.CreateThread(function()
+            Citizen.Wait(GlobalState.Timer * 60 * 1000)
+            if isDead then
+                TriggerServerEvent('hospital:server:RespawnAtHospital')
             end
-			
-            SetEntityInvincible(player, true)
-            SetEntityHealth(player, GetEntityMaxHealth(player))
-            if IsPedInAnyVehicle(player, false) then
-                loadAnimDict("veh@low@front_ps@idle_duck")
-                TaskPlayAnim(player, "veh@low@front_ps@idle_duck", "sit", 1.0, 1.0, -1, 1, 0, 0, 0, 0)
-            else
-                loadAnimDict(deadAnimDict)
-                TaskPlayAnim(player, deadAnimDict, deadAnim, 1.0, 1.0, -1, 1, 0, 0, 0, 0)
-            end
-        end
+        end)
     end
-end
+end)
